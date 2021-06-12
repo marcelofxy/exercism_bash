7cd6a8ebf8f2809994d31bbd211d9f7e99fc6885
marcelofxy <31291445+marcelofxy@users.noreply.github.com>
2021-06-12 13:42:43 -0300

    Update two_fer.sh

--- two_fer.sh
+++ two_fer.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-nameone="Alice"
-nametwo="Bob"
-namethr="Zaphod"
+nameone='Alice'
+nametwo='Bob'
+namethr='Zaphod'
 
 echo "One for $nameone, one for me"
 echo "One for $nametwo, one for me"
 echo "One for $namethr, one for me"
+echo ' '
+echo 'One for '$nameone', one for me'
+echo 'One for '$nametwo', one for me'
+echo 'One for '$namethr', one for me'
