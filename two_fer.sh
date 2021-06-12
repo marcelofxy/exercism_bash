67565bd326c01f982f2540fcb49c7fbf85390a3f
marcelofxy <31291445+marcelofxy@users.noreply.github.com>
2021-06-12 13:38:28 -0300

    Create two_fer.sh

---
+++ two_fer.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+nameone="Alice"
+nametwo="Bob"
+namethr="Zaphod"
+
+echo "One for $nameone, one for me"
+echo "One for $nametwo, one for me"
+echo "One for $namethr, one for me"
